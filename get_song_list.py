36298c04147b6f41491306cd1ef53483cf7ea2b7
mingtong wu <shenmizhe1906@gmail.com>
2018-04-09 09:29:44 -0400

    uasable

--- get_song_list.py
+++ get_song_list.py
@@ -6,8 +6,12 @@ from datetime import datetime
 import math
 import socket  
 import time  
-  
-  
+from pydub import AudioSegment
+import re
+import pafy
+import os
+import threading
+
 def ensureUtf(s):
   try:
       if type(s) == unicode:
@@ -28,13 +32,72 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 count_group =  soup.select('div a.pl-video-title-link')
 
-for ele in count_group:
+playList = []
+directory = r'C:\Users\Logan\Desktop\mp3_auto_sync'
+for filename in os.listdir(directory):
+	if filename.endswith(".mp3") or filename.endswith('.webm'):
+		playList.append(filename)
+def download_change_format(ele):
 	name = ele.get_text().strip()
 	url ='https://www.youtube.com' + ele['href']
+	video = pafy.new(url)
+	bestaudio = video.getbestaudio()
+	format = re.search('audio:(.*)@.*',str(bestaudio))
+	format = format.group(1)
+	filename = video.title.strip()
+	if str(filename +'.mp3') in playList or str(filename +'.webm') in playList:
+		print(filename + 'is already download')
+		return
+	for i in range(2):
+		try:
+			bestaudio.download()
+			break
+		except:
+			print(filename + 'download error')
+			continue
+			
+	for i in range(2):
+		try:
+			other = AudioSegment.from_file(filename +'.' + format, format)
+			break
+		except:
+			print(filename + 'read format error')
+			time.sleep(1)
+			continue
+	for i in range(2):
+		try:
+			other.export(filename + '.mp3', format = 'mp3')
+			print('finish ' + filename)
+			break
+		except:
+			print(filename + 'exprot error')
+			continue
+	for i in range(2):
+		try:
+			os.remove(filename +'.' + format)
+			break
+		except:
+			print(filename + 'remove error')
+			continue
+	
 
-	with open(filename+'.csv', 'a', newline='', encoding='utf-8') as csvfile:
-		spamwriter = csv.writer(csvfile)
-		list1 = [name, url]
-		list1 = list(map(lambda x:ensureUtf(x), list1))
-		print(list1)
-		spamwriter.writerow(list1)
+for ele in count_group:
+	download_change_format(ele)
+
+#change rest
+for filename in os.listdir(directory):
+	if filename.endswith(".webm"):
+		try:
+			other = AudioSegment.from_file(filename , format)
+			for i in range(2):
+				try:
+					other.export(filename[:-5] + '.mp3', format = 'mp3')	
+					os.remove(filename)
+					break
+				except:
+					print(filename + 'second time change format error')
+					continue
+		except:
+			pass
+	elif filename.endswith('.temp'):
+		os.remove(filename);
---
+++ handle_rest.py
@@ -0,0 +1,31 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+import math
+import socket  
+import time  
+from pydub import AudioSegment
+import re
+import pafy
+import os
+import threading
+
+directory = r'C:\Users\Logan\Desktop\mp3_auto_sync'
+
+#change rest
+for filename in os.listdir(directory):
+	if filename.endswith(".webm"):
+		print(filename + '  changing')
+		other = AudioSegment.from_file(filename , 'webm')
+		for i in range(2):
+			try:
+				other.export(filename[:-5] + '.mp3', format = 'mp3')	
+				os.remove(filename)
+				break
+			except:
+				print(filename + 'second time change format error')
+				continue
+	elif filename.endswith('.temp'):
+		os.remove(filename);
